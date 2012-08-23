61859cd7f36bf5b3f8ddb748e7b2a448252445fa
frankee <frankee.zhou@gmail.com>
2012-08-23 17:41:38 +0800

    cetty: delete useless test case.

--- cetty-core/test/cetty/buffer/TruncatedChannelBufferTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "cetty/buffer/TruncatedChannelBufferTest.h"
-
-using namespace cetty::buffer;
-
-#define CHANNEL_BUFFER_IMPL_TEST TruncatedChannelBufferTest
-#include "cetty/buffer/AbstractChannelBufferTest.inc.h"
