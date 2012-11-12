3fcfd79fe63b7809e46db0ee03982a63ec3ad345
chenhl <chenhl@chenhl-HP.(none)>
2012-11-12 16:34:06 +0800

    modify pipe.cpp

--- cetty-zurg/src/cetty/zurg/slave/Pipe.cpp
+++ cetty-zurg/src/cetty/zurg/slave/Pipe.cpp
@@ -6,14 +6,19 @@
  */
 
 #include <cetty/zurg/slave/Pipe.h>
+#include <cetty/zurg/Util.h>
 #include <stdexcept>
 
 namespace cetty {
 namespace zurg {
 namespace slave {
 
+using namespace cetty::zurg;
+
 Pipe::Pipe(){
-    if (::pipe2(pipefd_, O_CLOEXEC)) {
+    if (::pipe(pipefd_)) {
+        setFd(pipefd_[0], FD_CLOEXEC);
+        setFd(pipefd_[1], FD_CLOEXEC);
         char buf[512];
         throw std::runtime_error(strerror_r(errno, buf, sizeof buf));
     }
